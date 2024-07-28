81d24e3d4a8f6d895966be88fceb78ca696bcdd5
Lars van der Niet <lvdnbusiness@gmail.com>
2024-07-29 00:05:10 +0200

    Added PWA

---
+++ app/offline/page.tsx
@@ -0,0 +1,12 @@
+export default function OfflinePage() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        You seem to be offline
+      </h1>
+      <p className="mb-4">
+        Please check your internet connection and try again.
+      </p>
+    </section>
+  );
+}
---
+++ next.config.js
@@ -0,0 +1,27 @@
+const withPWA = require("@ducanh2912/next-pwa").default({
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  dest: "public",
+  fallbacks: {
+    //image: "/static/images/fallback.png",
+    document: "/offline",
+    // font: '/static/font/fallback.woff2',
+    // audio: ...,
+    // video: ...,
+  },
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["larsniet.com"],
+  },
+};
+
+module.exports = withPWA(nextConfig);
