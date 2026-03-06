1df44d39c5efa094b4f25eb0c0a3425984804ef8
Lars van der Niet <lvdnbusiness@icloud.com>
2026-03-06 15:51:31 +0100

    Add Apple Web App metadata and update theme color in manifest for improved PWA support

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,11 @@ export const viewport: Viewport = {
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "black-translucent",
+    title: "Lars van der Niet",
+  },
   alternates: {
     canonical: "/",
     types: {
